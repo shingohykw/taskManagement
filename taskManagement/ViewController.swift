c1751667721175d5917fe31894380e83ca17d8f4
Shingo Hayakawa <shingohykw@gmail.com>
2021-09-22 22:09:01 +0900

    Initial Commit

---
+++ taskManagement/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  taskManagement
+//
+//  Created by シンゴ on 2021/09/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
